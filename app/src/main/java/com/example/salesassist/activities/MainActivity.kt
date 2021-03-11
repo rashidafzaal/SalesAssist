4c307fd9c4e74f58346ae81c305feda96bd88f8f
Rashid Afzaal <37862752+rashidafzaal@users.noreply.github.com>
2021-03-11 19:08:29 +0500

    - combine navbar and sidebar

--- app/src/main/java/com/example/salesassist/activities/MainActivity.kt
+++ app/src/main/java/com/example/salesassist/activities/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.example.salesassist.activities
 
 import android.os.Bundle
+import android.view.Menu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.salesassist.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerAppBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+//        val toolbar: Toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
 
+        //============================= Bottom Navigation ==========================================================
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -28,5 +39,28 @@ class MainActivity : AppCompatActivity() {
         ))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //============================= Sidebar Drawer Navigation ==========================================================
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val drawerNavView: NavigationView = findViewById(R.id.navigationViewDrawer)
+        val drawerNavController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        drawerAppBarConfiguration = AppBarConfiguration(setOf(
+                R.id.nav_home), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(drawerAppBarConfiguration) || super.onSupportNavigateUp()
     }
 }
